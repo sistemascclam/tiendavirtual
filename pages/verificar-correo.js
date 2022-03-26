4f469cebb3d491040875bd7a58e64003d801d293
Daniela Paiva <danielapv200@gmail.com>
2022-03-25 23:42:54 -0500

    correo

--- pages/verificar-correo.js
+++ pages/verificar-correo.js
@@ -3,24 +3,22 @@ import React from 'react'
 
 export default function VerificarCorreo() {
   return (
-    <body className='bg-darkblue'>
-      <div className='flex flex-col mx-auto'>
-        <div className='tracking-wide l left-1/2 text-white font-bold py-8 absolute top-36 text-2xl'>
-          Revisa tu bandeja de entrada
-        </div>
-        <div className='tracking-wide  text-white left-1/2 py-14 absolute top-40'>Un link de inicio de secion se ha enviado a
-          <br />tu direccion de correo.</div>
-
-        <Link href="/">
-          <button className="bg-blue-600 w-72 h-10 rounded-full absolute top-1/2 left-1/2">
-            <a className='text-white text-xl tracking-wide'>
+    <body className='bg-mk-blue flex justify-center w-full'>
+      <div className='flex justify-center items-center  mt-32 gap-6'>
+        <img className=" w-64 left-52 top-36" src='../images/3d-fluency-blue-paper-plane.png'>
+        </img>
+        <div className='flex flex-col'>
+          <p className='text-white font-bold py-3 top-36 text-2xl'>
+            Revisa tu bandeja de entrada
+          </p>
+          <p className='tracking-wide  text-white '>Un link de inicio de secion se ha enviado a
+            <br />tu direccion de correo.</p>
+          <Link href="/">
+            <a className="mt-10 bg-white w-72 h-10 rounded-full top-1/2 left-1/2 text-mk-blue text-xl font-medium shadow-md flex justify-center items-center">
               Ir a la pagina principal
             </a>
-          </button>
-        </Link>
-
-        <img className=" w-96 h-90 left-52 absolute top-36" src='../images/3d-fluency-blue-paper-plane.png'>
-        </img>
+          </Link>
+        </div>
       </div>
     </body>
   )
